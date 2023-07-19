14f80b533c0d9f2568e91551e3cec8747489d1f3
NaveenKumarSubramanian <106928795+NaveenKumarSubramanian@users.noreply.github.com>
2023-07-19 15:17:52 +0530

    both

--- com.Thanvik/src/test/java/com/thanvik/LastNew.java
+++ com.Thanvik/src/test/java/com/thanvik/LastNew.java
@@ -3,7 +3,7 @@ package com.thanvik;
 public class LastNew {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("grate");	// TODO Auto-generated method stub
 
 	}
 
