a7b3188325a6d8d8a2c9fa47e32dd308fc65bbf4
NaveenKumarSubramanian <106928795+NaveenKumarSubramanian@users.noreply.github.com>
2023-07-19 18:03:54 +0530

    four

--- com.Thanvik/src/test/java/com/thanvik/Mine.java
+++ com.Thanvik/src/test/java/com/thanvik/Mine.java
@@ -4,7 +4,8 @@ public class Mine {
 
 	public static void main(String[] args) {
 		System.out.println("one");// TODO Auto-generated method stub
-System.out.println("two");
+System.out.println("three");
+System.out.println("four");
 	}
 
 }
